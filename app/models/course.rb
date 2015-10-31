b31789f5cad4d53e45d2aa367f9e7d94849dadd7
Matt Schmitter <matt.schmitter@gmail.com>
2015-10-31 01:19:27 +0000

    Added courses page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
+
+	validates :title, :presence => {:message => "Title is required"}
+	validates :description, :presence => {:message => "Description is required"}
+	validates :cost, :presence => {:message => "Cost is required"}, :numericality => {:greater_than_or_equal_to => 0}
 end
