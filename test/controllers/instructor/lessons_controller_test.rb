13abec92ab1ccfef7300dac8edab322ecf55547a
Matt Schmitter <matt.schmitter@gmail.com>
2015-11-07 21:15:14 +0000

    Added lesson permissions

--- test/controllers/instructor/lessons_controller_test.rb
+++ test/controllers/instructor/lessons_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Instructor::LessonsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+# Look up the section in the database the lesson is for.
+# Create the lesson in the database and connect it to the section we looked up.
+# Redirect the user to the instructor dashboard.
 end
